bcbda0833415d1c18268a257e05741867e5fc9d1
JustmaaZ <khanmaaz818@gmail.com.com>
2023-10-12 16:29:12 +0530

    add readme.md

---
+++ README.md
@@ -0,0 +1,1 @@
+### Electra Touch Repo
--- enroll.ino
+++ enroll.ino
@@ -1,19 +1,3 @@
-/***************************************************
-  This is an example sketch for our optical Fingerprint sensor
-
-  Designed specifically to work with the Adafruit BMP085 Breakout
-  ----> http://www.adafruit.com/products/751
-
-  These displays use TTL Serial to communicate, 2 pins are required to
-  interface
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
 #include <Adafruit_Fingerprint.h>
 
 
